26d491049cea5781c0ac99132836a477ef661b47
GaGiiiii <dragoslav.gagi8@gmail.com>
2021-10-02 21:27:00 +0200

    Finished Index Page

--- techzzy_front/src/components/Search/Search.js
+++ techzzy_front/src/components/Search/Search.js
@@ -1,25 +1,52 @@
-import React from 'react'
+import React, { useEffect } from 'react'
 import './search.css';
 import { useState } from 'react';
 
 export default function Search({ products }) {
   const [input, setInput] = useState("");
+  const [filteredProducts, setFilteredProducts] = useState([]);
+
+  useEffect(() => {
+    setFilteredProducts(filterProducts(products, input));
+    console.log(input);
+  }, [input])
+
+  function filterProducts(products, input) {
+    let filteredProducts = [...products];
+    filteredProducts = filteredProducts.filter((product) => product.name.toUpperCase().includes(input.toUpperCase()));
+
+    return filteredProducts;
+  }
+
+  function calculateProductRating(product) {
+    let rating = 0;
+
+    for (let i = 0; i < product.ratings.length; i++) {
+      rating += product.ratings[i].rating;
+    }
+
+    return (Math.round(rating / product.ratings.length * 100) / 100) || 0;
+  }
 
   return (
     <div>
       <div id="search">
         <input className="form-control" type="search" placeholder="Search" aria-label="Search" value={input} onChange={(e) => setInput(e.target.value)} />
         <div id="search-results">
-          <div className="search-result-item">
-            <div className="search-result-item-img">
-              <img src="https://cdn.pixabay.com/photo/2015/04/23/22/00/tree-736885__480.jpg" alt="" />
-            </div>
-            <div className="search-result-item-info">
-              <p>Ime | Kategorija</p>
-              <p>Ocena</p>
-              <p>Cena</p>
-            </div>
-          </div>
+          {input.length > 0 && filteredProducts.map((product) => (
+            <a href={`products/${product.id}`} key={product.id}>
+              <div className="search-result-item">
+                <div className="search-result-item-img">
+                  <img src={product.img} alt="" />
+                </div>
+                <div className="search-result-item-info">
+                  <p className="m-0">{product.name} | {product.category.name}</p>
+                  <p className="m-0">{calculateProductRating(product)} / 10</p>
+                  <p className="m-0">{(Math.round(product.price * 100) / 100).toLocaleString()}</p>
+                </div>
+              </div>
+            </a>
+          ))}
         </div>
       </div>
     </div>
